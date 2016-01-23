427454a6458700cccad601a9764928fea82bf904
nihaue <nickh@quicklearn.com>
2016-01-22 17:18:11 -0800

    Updated nuspec package details and project version numbers per updates for #5

--- Source/TRex.Metadata.Attributes/Properties/AssemblyInfo.cs
+++ Source/TRex.Metadata.Attributes/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("QuickLearn Training")]
 [assembly: AssemblyProduct("T-Rex Metadata Library (Attributes Only)")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: CLSCompliant(true)]
@@ -24,5 +24,5 @@ using System;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.1.7.0")]
-[assembly: AssemblyFileVersion("0.1.7.0")]
+[assembly: AssemblyVersion("0.2.0.0")]
+[assembly: AssemblyFileVersion("0.2.0.0")]
